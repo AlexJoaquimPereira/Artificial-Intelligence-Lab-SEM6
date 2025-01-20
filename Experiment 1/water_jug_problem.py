e32aa3b3d87cec171408091b78983268f1282834
AlexJoaquimPereira <alexpereira1781@gmail.com>
2025-01-20 11:21:51 +0530

    Water jug problem

---
+++ Experiment 1/water_jug_problem.py
@@ -0,0 +1,68 @@
+from collections import deque
+import numpy as np
+
+def bfs_rule(next:list, visited:set):
+    """
+    Generate all possible states from the current state
+    """
+    x, y = next
+    possible_states = []
+    
+    # fill jug A
+    if (4, y) not in visited: 
+        possible_states.append(4, y)
+    
+    # empty jug A
+    if (0, y) not in visited: 
+        possible_states.append(0, y)
+    
+    # fill jug B
+    if (x, 3) not in visited: 
+        possible_states.append(4, y)
+    
+    # empty jug B
+    if (x, 0) not in visited: 
+        possible_states.append(0, y)
+        
+    # if x + y > 3
+    if (x+y>3, y) not in visited:
+        possible_states.append(x+y-3, 3)
+    
+    # if x + y <= 3
+    if (x+y<=3, y) not in visited:
+        possible_states.append(0, x+y)
+        
+    # if x + y > 4
+    if (x+y>3, y) not in visited:
+        possible_states.append(4, x+y-4)
+    
+    # if x + y <= 4
+    if (x+y<=4, y) not in visited:
+        possible_states.append(x+y, 0)
+    
+
+def bfs(a:int, b:int):
+    '''
+    Breadth First Search adapted for water jug problem
+    '''
+    queue = deque([a, b])
+    visited = set()
+    visited.add([a, b])
+    path = list()
+    
+    while queue:
+        next = queue.popleft()
+        path = path + next + "->"
+        if next == [2, 0]: return path
+        
+        for new in bfs_rule(next, visited):
+            if new not in visited:
+                queue.append(new)
+                visited.add(new)
+        
+
+if __name__ == "__main__":
+    jug_a = 0
+    jug_b = 0
+    bfs(jug_a, jug_b)
+    
