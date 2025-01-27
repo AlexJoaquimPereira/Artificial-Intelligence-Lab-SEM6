01ad517e8bcf4309986395e93c0f5d55419077e8
AlexJoaquimPereira <alexpereira1781@gmail.com>
2025-01-27 09:56:08 +0530

    Water jug problem
    4-3 jug problem

--- Experiment 1/water_jug_problem.py
+++ Experiment 1/water_jug_problem.py
@@ -1,7 +1,7 @@
 from collections import deque
 import numpy as np
 
-def bfs_rule(next:list, visited:set):
+def bfs_rule(next, visited):
     """
     Generate all possible states from the current state
     """
@@ -10,59 +10,65 @@ def bfs_rule(next:list, visited:set):
     
     # fill jug A
     if (4, y) not in visited: 
-        possible_states.append(4, y)
+        possible_states.append((4, y))
     
     # empty jug A
     if (0, y) not in visited: 
-        possible_states.append(0, y)
+        possible_states.append((0, y))
     
     # fill jug B
     if (x, 3) not in visited: 
-        possible_states.append(4, y)
+        possible_states.append((x, 3))
     
     # empty jug B
     if (x, 0) not in visited: 
-        possible_states.append(0, y)
+        possible_states.append((x, 0))
         
     # if x + y > 3
-    if (x+y>3, y) not in visited:
-        possible_states.append(x+y-3, 3)
+    if x + y > 3 and (x + y - 3, 3) not in visited:
+        possible_states.append((x + y - 3, 3))
     
     # if x + y <= 3
-    if (x+y<=3, y) not in visited:
-        possible_states.append(0, x+y)
+    if x + y <= 3 and (0, x + y) not in visited:
+        possible_states.append((0, x + y))
         
     # if x + y > 4
-    if (x+y>3, y) not in visited:
-        possible_states.append(4, x+y-4)
+    if x + y > 4 and (4, x + y - 4) not in visited:
+        possible_states.append((4, x + y - 4))
     
     # if x + y <= 4
-    if (x+y<=4, y) not in visited:
-        possible_states.append(x+y, 0)
+    if x + y <= 4 and (x + y, 0) not in visited:
+        possible_states.append((x + y, 0))
+        
+    return possible_states
     
 
 def bfs(a:int, b:int):
     '''
     Breadth First Search adapted for water jug problem
     '''
-    queue = deque([a, b])
+    queue = deque([(a, b)])
     visited = set()
-    visited.add([a, b])
-    path = list()
+    visited.add((a, b))
+    path = []
     
     while queue:
         next = queue.popleft()
-        path = path + next + "->"
-        if next == [2, 0]: return path
+        path.append(next)
+        if next == (2, 0): 
+            return path
         
         for new in bfs_rule(next, visited):
             if new not in visited:
                 queue.append(new)
                 visited.add(new)
+    
+    return None
         
 
 if __name__ == "__main__":
     jug_a = 0
     jug_b = 0
-    bfs(jug_a, jug_b)
-    
+    capacity_a = int(input("Capacity of jug A: "))
+    capacity_b = int(input("Capacity of jug b: "))
+    print("Traversed path: ", bfs(jug_a, jug_b))
