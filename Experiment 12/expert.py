519dc6f8938dff740ae71861dcf2bca904965850
AlexJoaquimPereira <alexpereira1781@gmail.com>
2025-05-05 01:13:24 +0530

    Expert system for medicines This is a preliminary attempt

---
+++ Experiment 12/expert.py
@@ -0,0 +1,21 @@
+# Expert System for diagnosing diseases
+# Use if-else statements or similar to differentiate btwn similar diseases
+
+# The expert system
+# Dict of lists of symptoms
+medical_expert = {
+    'covid-19': [],
+    'flu': [],
+    'common_cold': [],
+    'dengue': [],
+    'malaria': [],
+}
+
+def mediexpert():
+    '''
+    Medial expert system for diagnosing diseases
+    '''
+    
+
+if __name__ == "__main__":
+    mediexpert()
